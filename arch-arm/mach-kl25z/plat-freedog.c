4e069650f789e470959125eccd6281d13bb64b25
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2015-01-10 10:21:42 +0100

    KL25Z: fixed mbed adc driver pinout
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-arm/mach-kl25z/plat-freedog.c
+++ arch-arm/mach-kl25z/plat-freedog.c
@@ -11,26 +11,26 @@
 
 /* ADCs */
 
-#define VRES_UV (5000000 / (1 << 10))
+#define VRES_UV (3300000 / (1 << 16))
 
 #ifdef CONFIG_MBED_DRIVERS
 
 #include <analogin_api.h>
 
 const struct adc adcs[] = {
-	BATHOS_ADC((uint8_t)PTE30, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A0 */
-	BATHOS_ADC((uint8_t)PTE29, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A1 */
-	BATHOS_ADC((uint8_t)PTE24, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A2 */
-	BATHOS_ADC((uint8_t)PTE25, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A3 */
-	BATHOS_ADC((uint8_t)PTE21, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A4 */
-	BATHOS_ADC((uint8_t)PTE20, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A5 */
+	BATHOS_ADC((uint8_t)PTB0, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A0 */
+	BATHOS_ADC((uint8_t)PTB1, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A1 */
+	BATHOS_ADC((uint8_t)PTB2, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A2 */
+	BATHOS_ADC((uint8_t)PTB3, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A3 */
+	BATHOS_ADC((uint8_t)PTC2, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A4 */
+	BATHOS_ADC((uint8_t)PTC1, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A5 */
 };
+
+#endif
 
 const uint32_t num_adc = ARRAY_SIZE(adcs);
 const uint32_t min_per = (1000000 / HZ);
 const uint32_t max_mul = 0xffffffff;
-
-#endif
 
 /* GPIOs */
 char gpio_labels_start[MCUIO_NGPIO * 4];
